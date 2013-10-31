60a63006a153b9418771bb182e52697209697b2b
Nop Jiarathanakul <prutsdomj@gmail.com>
2013-10-31 01:55:00 -0700

    slow button only slow aim

--- app/js/mycontrols.js
+++ app/js/mycontrols.js
@@ -10,7 +10,7 @@ define([
   var PITCH_SPEED = Math.PI * 1/8;
   var MOVE_SPEED = 100;
   var BOOST_FACTOR = 5;
-  var SLOW_FACTOR = 0.25;
+  var SLOW_AIM_FACTOR = 0.2;
 
   var MyControls = {
     speed: new THREE.Vector3(),
@@ -30,38 +30,39 @@ define([
     update: function(delta) {
       this.gamepad.update();
 
-      var speedMult = 1;
+      var boostMult = 1;
+      var slowAimMult = 1;
 
       if (this.gamepad.buttons["LEFT_SHOULDER_BOTTOM"])
-        speedMult *= BOOST_FACTOR;
+        boostMult *= BOOST_FACTOR;
 
       if (this.gamepad.buttons["LEFT_SHOULDER"])
-        speedMult *= SLOW_FACTOR;
+        slowAimMult *= SLOW_AIM_FACTOR;
 
       var lookV = this.gamepad.axes["LEFT_ANALOGUE_VERT"];
-      this.controls.pitchSpeed = PITCH_SPEED * Math.abs(lookV) * speedMult;
+      this.controls.pitchSpeed = PITCH_SPEED * Math.abs(lookV) * boostMult * slowAimMult;
       this.controls.moveState.pitchUp = (lookV < -ANALOG1_TH) * 1;
       this.controls.moveState.pitchDown = (lookV > ANALOG1_TH) * 1;
 
       var lookH = this.gamepad.axes["LEFT_ANALOGUE_HOR"];
-      this.controls.rollSpeed = ROLL_SPEED * Math.abs(lookH) * speedMult;
+      this.controls.rollSpeed = ROLL_SPEED * Math.abs(lookH) * boostMult * slowAimMult;
       this.controls.moveState.rollLeft = (lookH < -ANALOG1_TH) * 1;
       this.controls.moveState.rollRight = (lookH > ANALOG1_TH) * 1;
 
       var moveV = this.gamepad.axes["RIGHT_ANALOGUE_VERT"];
       if (Math.abs(moveV) > ANALOG2_TH)
-        this.speed.z += MOVE_SPEED * delta * moveV * speedMult;
+        this.speed.z += MOVE_SPEED * delta * moveV * boostMult;
 
       var moveH = this.gamepad.axes["RIGHT_ANALOGUE_HOR"];
       if (Math.abs(moveH) > ANALOG2_TH)
-        this.speed.x += MOVE_SPEED * delta * moveH * speedMult;
+        this.speed.x += MOVE_SPEED * delta * moveH * boostMult;
 
       if (this.gamepad.buttons["RIGHT_ANALOGUE_STICK"]) {
         this.speed.x = 0;
         this.speed.z = 0;
       }
 
-      this.controls.moveVector = this.speed.clone().multiplyScalar(speedMult);
+      this.controls.moveVector = this.speed.clone().multiplyScalar(boostMult);
 
       this.controls.update(delta);
     }
